62baec2078a1199c3ae1f5037d43f9837ed94ead
leguan8 <38935437+leguan8@users.noreply.github.com>
2025-07-01 14:42:55 +0800

    Delete get_artworks.php

--- get_artworks.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$f='data/artworks.json';
-if(!file_exists($f)){ echo '[]'; exit; }
-$json=file_get_contents($f);
-$out=json_decode($json,true);
-echo json_encode($out,JSON_UNESCAPED_UNICODE);
